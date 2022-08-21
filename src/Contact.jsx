6bcdc26b303329fe84292b02e4921f21f5e161df
ProIQ <codingmonk3@gmail.com>
2022-08-21 16:08:23 +0530

    my-second-commit-for-boni-landing-page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -7,9 +7,28 @@ const Contact = () => {
             Contact us
         </div>
         <div className="contact-details">
-
+            <div className="say-hi">Send us a ‘Hi’ to get registered or for help</div>
+            <div className="mess"><span className="busy">Hi Boni Business</span><span className="time">12:00pm</span></div>
+            <button className="what-app">
+                <div className="kit"></div>
+                <div className="phone"></div>
+                Say Hi Boni Business
+            </button>
+            <div className="or">Or</div>
+            <div className="phone-number">
+              <div className="tell-number">
+              Contact us at
+              </div>
+              <button className="tell-no">
+                <div className="callcentre">
+                  
+                </div>
+                9019516735
+              </button>
+            </div>
         </div>
         <div className="oongli-1"></div>
+        <div className="oongli-2"></div>
     </div>
   )
 }
